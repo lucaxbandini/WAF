7c8fe4f681d0b592b776dd0285fad83499671d76
Luca Bandini <lucaxbandini@gmail.com>
2024-08-29 20:45:46 +0200

    added alb, listener

--- main.tf
+++ main.tf
@@ -51,7 +51,16 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "LucaWAF"
+    Name = "LucaWAFvpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "LucaWAFsn"
   }
 }
 
@@ -87,4 +96,31 @@ resource "aws_lb_target_group" "LB-WAF-TG" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb" "LB-WAF-ALB" {
+  name               = "LB-WAF-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.LBserverSG]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "LB-WAF-ALB"
+  }
+}
+
+resource "aws_lb_listener" "LB-WAF-ALB" {
+  load_balancer_arn = aws_lb.LB-WAF-ALB.arn
+  port              = "8080"
+  protocol          = "HTTP"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.LB-WAF-TG.arn
+  }
 }
