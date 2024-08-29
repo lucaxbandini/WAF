0e3ae7e0ff978c55a9bd3eca0b36f4d41d9b97b2
Luca Bandini <lucaxbandini@gmail.com>
2024-08-29 20:22:37 +0200

    added userdata, group rule additions

--- main.tf
+++ main.tf
@@ -6,6 +6,17 @@ resource "aws_instance" "LBWebServer1" {
   ami           = "ami-02c21308fed24a8ab"
   instance_type = "t2.micro"
   key_name      = "LBserverKP"
+  associate_public_ip_address = true
+  security_groups = LBserverSG
+  user_data = <<EOF
+"#!/bin/bash
+sudo su
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "<html><h1> Welcome to Luca's 1st server! </h1><html>" >> /var/www/html/index.html"
+EOF
   tags = {
     name = "LBWebServer1"
   }
@@ -15,6 +26,17 @@ resource "aws_instance" "LBWebServer2" {
   ami           = "ami-02c21308fed24a8ab"
   instance_type = "t2.micro"
   key_name      = "LBserverKP"
+  associate_public_ip_address = true
+  security_groups = LBserverSG
+  user_data = <<EOF
+  #!/bin/bash
+  sudo su
+  yum update -y
+  yum install httpd -y
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<html><h1> Welcome to Luca's 2nd server! </h1><html>" >> /var/www/html/index.html"
+  EOF
   tags = {
     name = "LBWebServer2"
   }
@@ -22,31 +44,42 @@ resource "aws_instance" "LBWebServer2" {
 
 resource "aws_security_group" "LBserverSG" {
   description = "Luca's Server SG"
-   tags = {
+  tags = {
     Name = "LBserverSG"
   }
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "LucaWAF"
+  }
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
+  security_group_id = aws_security_group.LBserverSG.id
   cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 0
+  from_port         = 22
   ip_protocol       = "ssh"
   to_port           = 22
+  description       = "allows ssh"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
-  security_group_id = aws_security_group.allow_http.id
+  security_group_id = aws_security_group.LBserverSG.id
   cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 0
+  from_port         = 8080
   ip_protocol       = "http"
   to_port           = 8080
+  description       = "allows http"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
-  security_group_id = aws_security_group.allow_https.id
+  security_group_id = aws_security_group.LBserverSG.id
   cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 0
+  from_port         = 443
   ip_protocol       = "https"
   to_port           = 443
+  description       = "allows https"
 }
