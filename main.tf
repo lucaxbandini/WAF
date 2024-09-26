125022506abf1b527b31fe5ef8eaec4daf2cde69
Luca Bandini <lucaxbandini@gmail.com>
2024-09-26 15:06:49 -0400

    more tweaks

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "LBWebServer1" {
@@ -14,7 +14,7 @@ resource "aws_instance" "LBWebServer1" {
   }
 
   user_data = <<EOF
-  "#!/bin/bash
+  #!/bin/bash
   sudo su
   yum update -y
   yum install httpd -y
@@ -76,25 +76,28 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "LucaWAFsn1"
   }
 }
 
 resource "aws_subnet" "public2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "LucaWAFsn2"
   }
 }
 
 resource "aws_network_interface" "server1" {
-  subnet_id = aws_subnet.public.id
+  subnet_id       = aws_subnet.public.id
+  security_groups = [aws_security_group.LBserverSG.id]
 
   tags = {
     Name = "primary_network_interface1"
@@ -102,7 +105,8 @@ resource "aws_network_interface" "server1" {
 }
 
 resource "aws_network_interface" "server2" {
-  subnet_id = aws_subnet.public.id
+  subnet_id       = aws_subnet.public2.id
+  security_groups = [aws_security_group.LBserverSG.id]
 
   tags = {
     Name = "primary_network_interface2"
@@ -116,6 +120,28 @@ resource "aws_internet_gateway" "gw" {
     Name = "main"
   }
 }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
 
 resource "aws_security_group_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.LBserverSG.id
@@ -125,6 +151,15 @@ resource "aws_security_group_rule" "allow_ssh_ipv4" {
   protocol          = "tcp"
   to_port           = 22
   description       = "allows ssh"
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.LBserverSG.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "allow_http_ipv4" {
@@ -148,21 +183,22 @@ resource "aws_security_group_rule" "lb_allow_http_ipv4" {
 }
 
 resource "aws_lb_target_group" "LB-WAF-TG" {
-  name     = "LB-WAF-TG"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "LB-WAF-TG"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 }
 
 resource "aws_lb_target_group_attachment" "LB-WAF-TG-EC2-1" {
   target_group_arn = aws_lb_target_group.LB-WAF-TG.arn
-  target_id        = aws_instance.LBWebServer1.id
+  target_id        = aws_network_interface.server1.private_ip
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "LB-WAF-TG-EC2-2" {
   target_group_arn = aws_lb_target_group.LB-WAF-TG.arn
-  target_id        = aws_instance.LBWebServer2.id
+  target_id        = aws_network_interface.server2.private_ip
   port             = 80
 }
 
@@ -174,7 +210,7 @@ resource "aws_lb" "LB-WAF-ALB" {
   security_groups = [aws_security_group.LBserverSG.id]
   subnets         = [aws_subnet.public.id, aws_subnet.public2.id]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Name = "LB-WAF-ALB"
@@ -216,9 +252,9 @@ resource "aws_wafv2_web_acl" "ruleset" {
     }
 
     visibility_config {
-      cloudwatch_metrics_enabled = false
+      cloudwatch_metrics_enabled = true
       metric_name                = "ip-reputation-metrics"
-      sampled_requests_enabled   = false
+      sampled_requests_enabled   = true
     }
   }
 
@@ -238,9 +274,9 @@ resource "aws_wafv2_web_acl" "ruleset" {
     }
 
     visibility_config {
-      cloudwatch_metrics_enabled = false
+      cloudwatch_metrics_enabled = true
       metric_name                = "anonymous-ip-metrics"
-      sampled_requests_enabled   = false
+      sampled_requests_enabled   = true
     }
   }
 
@@ -260,9 +296,9 @@ resource "aws_wafv2_web_acl" "ruleset" {
     }
 
     visibility_config {
-      cloudwatch_metrics_enabled = false
+      cloudwatch_metrics_enabled = true
       metric_name                = "core-rule-set-metrics"
-      sampled_requests_enabled   = false
+      sampled_requests_enabled   = true
     }
   }
 
@@ -282,9 +318,9 @@ resource "aws_wafv2_web_acl" "ruleset" {
     }
 
     visibility_config {
-      cloudwatch_metrics_enabled = false
+      cloudwatch_metrics_enabled = true
       metric_name                = "known-bad-inputs-metrics"
-      sampled_requests_enabled   = false
+      sampled_requests_enabled   = true
     }
   }
 
@@ -296,8 +332,8 @@ resource "aws_wafv2_web_acl" "ruleset" {
   token_domains = ["mywebsite.com", "myotherwebsite.com"]
 
   visibility_config {
-    cloudwatch_metrics_enabled = false
+    cloudwatch_metrics_enabled = true
     metric_name                = "web-acl-metrics"
-    sampled_requests_enabled   = false
+    sampled_requests_enabled   = true
   }
 }
